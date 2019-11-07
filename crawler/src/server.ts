1f5083b09e7d397c38b987bbf200575c4a80586e
Leo <leonardo.almeidamarques@gmail.com>
2019-11-07 09:22:13 +0100

    moved the app to another folder and created crawler

--- crawler/src/server.ts
+++ crawler/src/server.ts
@@ -1,12 +1,13 @@
 import fetch from 'node-fetch';
 const cheerio = require('cheerio');
+const cors = require('cors');
 const port = 3333;
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-
+app.use(cors());
 const getItems = async url => {
   const response = await fetch(url);
   const body = await response.text();
@@ -17,6 +18,10 @@ const getItems = async url => {
 app.post('/', async function(req, res) {
   const result = await getItems(req.body.url);
   res.send(result);
+});
+
+app.post('/place-order', async function(req, res) {
+  res.send(req.body);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
